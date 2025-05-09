1d6a1b852d70252da4a9f44812f9726c62bb26fb
La The Phong <latp1102@gmail.com>
2025-05-09 07:57:25 +0700

    09/05

--- SessionManager/js/pageChangeJpPrimary.js
+++ SessionManager/js/pageChangeJpPrimary.js
@@ -1,8 +1,8 @@
 const pages = [
     { id: "page-change-hiragana", url: "/team2-mankai-user/SessionManager/pages/jpPrimary_Hiragana.html" },
     { id: "page-change-katakana", url: "/team2-mankai-user/SessionManager/pages/jpPrimary_Katakana.html" },
-    { id: "page-change-test", url: "/team2-mankai-user/SessionManager/pages/jpPrimary_Test.html" },
-    { id: "page-change-score", url: "/team2-mankai-user/SessionManager/pages/jpPrimary_Score.html" }
+    { id: "page-change-score", url: "/team2-mankai-user/SessionManager/pages/jpPrimary_Score.html" },
+    { id: "page-change-test", url: "/team2-mankai-user/SessionManager/pages/jpPrimary_Test.html" }
 ];
 
 pages.forEach(page => {
