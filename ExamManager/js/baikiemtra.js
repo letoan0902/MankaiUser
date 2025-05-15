75581e0c758fa392125d575b05416b3ecf8fb868
Hoang Minh Quang <hoangminhquang1310@gmail.com>
2025-05-15 12:02:30 +0700

    15/05

--- ExamManager/js/baikiemtra.js
+++ ExamManager/js/baikiemtra.js
@@ -15,6 +15,11 @@ let trueAnswer = document.querySelector(".true");
 let falseAnswer = document.querySelector(".false");
 let questionComplete = document.querySelector(".question-complete");
 
+// bài kiểm tra đọc/nghe
+let containerButtonReadListen = document.querySelector(".container-button-read-listen");
+containerButtonReadListen.style.display = "flex";
+let containerExplainReadListen = document.querySelector(".container-explain-read-listen");
+containerExplainReadListen.style.display = "none";
 
 
 function renderQuestion(index) {
@@ -153,10 +158,6 @@ function checkAnswer() {
     let question = questions[indexQuestion];
     let userSelectedAnswerIndex = userSelection[indexQuestion];
 
-    if (!question) {
-        console.error("Câu hỏi không tồn tại.");
-        return;
-    }
     let correctAnswerIndex = question.select.findIndex((option) => option.check === true);
 
     if (userSelectedAnswerIndex === correctAnswerIndex) {
@@ -182,45 +183,120 @@ attachAnswerEvents();
 
 
 let contentExamReadListen = document.querySelector(".content-exam-read-listen");
+let indexQuestionExamReadListen = 0
 function renderExamReadListen(index){
-    let questions = user.course[0].lessons[0].detail[6].test[1].question;
-    let html = questions.map((group, groupIndex) => {
-        let groupHtml = `
-            
-                  <div class="text-exam-read-listen">
-                    <p class="question-complete-read-listen">Câu 1/6</p>
-                    <h3 class="title-read-listen">Nội dung đề bài đọc/bài nghe</h3>
-                    <p class="text-question-read-listen"><span class="special-read-listen">${group.special}</span>${group.text}</p>
-                    <img src="/team2-mankai-user/assets/icons/Listening.svg" alt="">
-                  </div>
-                
-                <div class="container-list-answer">
-                    ${group.list.map((data, index) => `
-                      <p class="question-read-listen">1. ${data.problem}</p>
-                      <div class="list-answer-read-listen">
-                        <div class="answer-item-read-listen">
-                            <span>A</span>
-                            <p>こんにちは</p>
-                        </div>
-                        <div class="answer-item-read-listen">
-                            <span>B</span>
-                            <p>こんにちは</p>
-                        </div>
-                        <div class="answer-item-read-listen">
-                            <span>C</span>
-                            <p>こんにちは</p>
-                        </div>
-                        <div class="answer-item-read-listen">
-                            <span>D</span>
-                            <p>こんにちは</p>
-                        </div>
-                    </div>
-                    `).join('')}
-                </div>
+    let questions = user.course[0].lessons[0].detail[6].test[1].question[index];
+    
+    let groupHtml = `
+    <div class="text-exam-read-listen">
+      <p class="question-complete-read-listen">Câu ${index + 1}/${user.course[0].lessons[0].detail[6].test[1].question.length}</p>
+      <h3 class="title-read-listen">Nội dung đề bài đọc/bài nghe</h3>
+      <p class="text-question-read-listen"><span class="special-read-listen">${questions.special}</span>${questions.text}</p>
+      <img src="/team2-mankai-user/assets/icons/Listening.svg" alt="">
+    </div>
+    <div class="container-list-answer">
+      ${questions.list.map((data, index) => `
+        <p class="question-read-listen">${index + 1}. ${data.problem}</p>
+        <div class="list-answer-read-listen">
+          <div class="answer-item-read-listen">
+            <span>A</span>
+            <p>${data.select[0].value}</p>
+          </div>
+          <div class="answer-item-read-listen">
+            <span>B</span>
+            <p>${data.select[1].value}</p>
+          </div>
+          <div class="answer-item-read-listen">
+            <span>C</span>
+            <p>${data.select[2].value}</p>
+          </div>
+          <div class="answer-item-read-listen">
+            <span>D</span>
+            <p>${data.select[3].value}</p>
+          </div>
+        </div>
+      `).join('')}
+    </div>
+  `;
 
-        `;
-        return groupHtml;
-    }).join('');
-
-    containerQuestion.innerHTML = html;
+  contentExamReadListen.innerHTML = groupHtml;
+  attachAnswerExamReadListen();
 }
+
+let checkReadListen = document.querySelector(".check-read-listen");
+let trueReadListen = document.querySelector(".true-read-listen");
+let falseReadListen = document.querySelector(".false-read-listen");
+let btnContinueReadListen = document.querySelector(".btn-continue-read-listen");
+checkReadListen.addEventListener("click",checkAnswerExamReadListen);
+
+
+btnContinueReadListen.addEventListener("click",function(){
+    indexQuestionExamReadListen++;
+    renderExamReadListen(indexQuestionExamReadListen);
+});
+
+let userSelectionExamReadListen = [];
+let isCheckedExamReadListen = false; 
+
+function attachAnswerExamReadListen() {
+    const containerListAnswers = document.querySelectorAll(".container-list-answer");
+    containerListAnswers.forEach((container) => {
+        const answerLists = container.querySelectorAll(".list-answer-read-listen");
+        answerLists.forEach((answerList, questionIndex) => {
+            const answerItems = answerList.querySelectorAll(".answer-item-read-listen");
+            answerItems.forEach((answer, answerIndex) => {
+                answer.addEventListener("click", function (event) {
+                    if (isCheckedExamReadListen){
+                        return;
+                    }
+                    answerItems.forEach((ans) => {
+                        ans.style.borderColor = "";
+                        ans.style.boxShadow = "";
+                    });
+
+                    event.currentTarget.style.borderColor = "#0BA5EC";
+                    event.currentTarget.style.boxShadow = "0px 4px 0px 0px #0BA5EC";
+                    userSelectionExamReadListen[questionIndex] = answerIndex;
+                });
+            });
+        });
+    });
+}
+
+function checkAnswerExamReadListen() {
+    if (userSelectionExamReadListen[indexQuestionExamReadListen] === undefined) {
+        Swal.fire({
+            title: "Chưa chọn đáp án?",
+            text: "Hãy chọn đáp án trước khi kiểm tra đúng sai?",
+            icon: "question"
+          });
+        return;
+    }
+
+    isChecked = true;
+    isCheckedExamReadListen[indexQuestionExamReadListen] = true;
+
+    let question = questions[indexQuestionExamReadListen];
+    let userSelectedAnswerIndex = userSelectionExamReadListen[indexQuestionExamReadListen];
+
+    let correctAnswerIndex = question.select.findIndex((option) => option.check === true);
+
+    if (userSelectedAnswerIndex === correctAnswerIndex) {
+        scoreExam1++; 
+        containerExplain.style.display = "block";
+        trueAnswer.style.display = "flex";
+        falseAnswer.style.display = "none";
+        containerButton.style.display = "none";
+    } else {
+        containerExplain.style.display = "block";
+        trueAnswer.style.display = "none";
+        falseAnswer.style.display = "flex";
+        containerButton.style.display = "none";
+    }
+
+    localStorage.setItem("scoreExamReadListen", scoreExamReadListen.toString());
+    console.log(`Điểm số hiện tại: ${scoreExamReadListen}/${questions.length}`);
+}
+
+renderExamReadListen(indexQuestionExamReadListen);
+
